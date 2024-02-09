3f6cff3c92a3291dbb83e0ce93f44ff7aaa08a6c
DarshanaJondhale <140425261+DarshanaJondhale@users.noreply.github.com>
2024-02-09 08:25:58 +0000

    dataTypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// alert("hello")
+
+console.log(3+3);
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); //null is a object
