88885f0461d4b1b8f3ebcc6a60d9f5d6832517a7
DarshanaJondhale <140425261+DarshanaJondhale@users.noreply.github.com>
2024-02-09 10:13:51 +0000

    updated with stack heap

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 2455768679804455n
 
@@ -31,4 +31,28 @@ const myFunction = function(){
     console.log("hello");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+// *****************************************************************************************************
+
+// stack (Primitive) , Heap (Non-Primitive)
+
+let myName = "Darshana"
+
+let anotherName = myName
+anotherName = "DJ"
+
+console.log(anotherName);
+console.log(myName);
+
+let userOne = {
+    email : "darshu@111",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "dd@1"
+
+console.log(userOne.email);
+console.log(userTwo.email);
