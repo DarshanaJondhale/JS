266c1987d439d556dd6f62f19de745222eaec2e0
DarshanaJondhale <140425261+DarshanaJondhale@users.noreply.github.com>
2024-02-09 09:18:13 +0000

    updated

--- 01_basics/03_conversionOrOperations.js
+++ 01_basics/03_conversionOrOperations.js
@@ -1,11 +1,11 @@
 let score = "Darshu"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" = NaN (typeof Na is number itself)
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +24,27 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************************* Operations ***************************************************
+
+let value = 3
+let negValue =-value
+// console.log(negValue);
+
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = " Darshu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 + 2);
+// console.log(1 + 2 +"2");
+
+console.log(+true);
+console.log(+"");
