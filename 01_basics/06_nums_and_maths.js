a49f075cbe0733e94d5a752131552e98482266fc
DarshanaJondhale <140425261+DarshanaJondhale@users.noreply.github.com>
2024-02-10 10:12:32 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,57 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //for fixed precision
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ******************************** maths ***************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(1,2,3,4,6,22));
+// console.log(Math.max(3,4,6,8,77,9,50));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
